9a6ec7736ad4d642177be6e78ed9b34dfc2bc255
Alex Calhoun <alxcalhoun@gmail.com>
2022-02-09 19:14:01 +0000

    updated with updateOne

--- app.js
+++ app.js
@@ -16,15 +16,23 @@ const app = async() =>{
             year:argv.year,
             oscar:argv.oscar
         })
+    } else if{
+
+     Movie.find({}).then(data => console.log(data))
+
     }
 
-     else if (
-        Movie.find({}).then(data => console.log(data))
-    )
+    else if {
+        Movie.find({year: {$gte:1980}}).then(data => console.log(data))
+    }
+
+    else if{
+        Movie.updateOne({title: "The Revenant"},{year: 2015}).then(res => console.log(res))
+    }
                 else{
                     res.send(data);
                 }
-            
+        
         
     else(
         console.log('wrong command')
